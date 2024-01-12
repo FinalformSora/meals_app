81518faf9ffc2eeedccaa06e5d3e4d17228d806e
Jesus Osuna <jesusgonzalez9664@gmail.com>
2024-01-12 00:55:10 -0800

    Add transparent_image dependency and implement category selection

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,9 +3,11 @@ import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
   final Category category;
+  final void Function() onSelectCategory;
 
-  const CategoryGridItem({Key? key, required this.category}) : super(key: key);
-
+  const CategoryGridItem(
+      {Key? key, required this.category, required this.onSelectCategory})
+      : super(key: key);
   // void selectCategory(BuildContext context) {
   //   Navigator.of(context).pushNamed(
   //     CategoryMealsScreen.routeName,
@@ -15,23 +17,29 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).colorScheme.onPrimary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
